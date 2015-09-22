e78c5e7f7ef40ad1a6edd422809e3f8ee65f97bf
Arquilino <arquilinocarlos@gmail.com>
2015-09-22 20:07:53 -0300

    Separado método before para testes do padrão Strategy

--- test/com/edupsousa/designpatterns/strategy/StrategyTest.java
+++ test/com/edupsousa/designpatterns/strategy/StrategyTest.java
@@ -2,10 +2,18 @@ package com.edupsousa.designpatterns.strategy;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StrategyTest {
-
+	ClasseComStrategy objeto;
+	
+	@Before
+	public void instaciarClasse(){
+		objeto = new ClasseComStrategy();
+	}
+	
+	
 	@Test
 	public void testarPadraoStrategy() {
 		ClasseComStrategy objeto = new ClasseComStrategy();
